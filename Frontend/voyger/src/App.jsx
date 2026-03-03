b6b58b97b2a18d2f6b1cc264c7f56d4d983e79bd
Harshal Sonune <harshalsonune55@gmail.com>
2026-03-03 22:48:26 +0530

    initial commit

---
+++ Frontend/voyger/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import StartupHero from './components/StartupHero'
+
+function App() {
+  return (
+    <>
+    <StartupHero/>
+    </>
+  )
+}
+
+export default App
